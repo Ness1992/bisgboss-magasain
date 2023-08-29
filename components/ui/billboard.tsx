e6074cdd45d5d3d9846dd94b4bc3b1241a0906c2
Ness1992 <127391994+Ness1992@users.noreply.github.com>
2023-08-29 21:34:05 +0200

    comit

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -4,21 +4,24 @@ interface BillboardProps {
   data: Billboard;
 }
 
-const Billboard: React.FC<BillboardProps> = ({
-  data
-}) => {
-  console
-  return ( 
+const BillboardComponent: React.FC<BillboardProps> = ({ data }) => {
+  return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+      <div
+        style={{
+          backgroundImage: `url(${data?.imageUrl})`,
+          filter: "brightness(70%)", // Ajuster la luminosité de l'image (par exemple, 70%)
+        }}
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+      >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs text-black">
             {data.label}
           </div>
         </div>
       </div>
     </div>
-   );
+  );
 };
 
-export default Billboard;
+export default BillboardComponent;
